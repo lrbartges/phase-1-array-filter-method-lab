12d9e7cf9eca584573b5f0dc82193d245cf20c8a
Lindsey Bartges <lrbartges@gmail.com>
2023-10-04 16:31:52 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function(driver) {
+        const lowerCaseDriver = driver.toLowerCase();
+        const lowerCaseName = name.toLowerCase();
+
+        if (lowerCaseDriver === lowerCaseName) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+}
+  
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function(driver) {
+        const lowerCaseDriver = driver.toLowerCase();
+        const lowerCaseLetters = letters.toLowerCase();
+
+        if (lowerCaseDriver.startsWith(lowerCaseLetters)) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+}
+  
+function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+        const lowerCaseDriverName = driver.name.toLowerCase();
+        const lowerCaseName = name.toLowerCase();
+
+        if (lowerCaseDriverName === lowerCaseName) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+}
+module.exports = { findMatching, fuzzyMatch, matchName };
